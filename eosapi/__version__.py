e6b77f052e9cec660629b643a8cfa7d6ece362b7
charlessimonyi <cfanhacker@gmail.com>
2022-06-04 01:02:59 +0800

    readme

--- eosapi/__version__.py
+++ eosapi/__version__.py
@@ -2,7 +2,7 @@ __title__ = "eosapi"
 __description__ = "a simple, high-level and lightweight eosio sdk write by python"
 __url__ = "https://github.com/encoderlee/eosapi"
 __author__ = "encoderlee"
-__author_email__ = "encoderlee@qq.com"
+__author_email__ = "encoderlee@gmail.com"
 __license__ = "MIT"
 
 __version__ = "1.0.0"
